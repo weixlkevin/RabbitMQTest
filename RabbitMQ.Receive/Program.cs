3a0783465cec1e63c8c2afeca58747342bf05e42
weixl <weixingliang@gci-china.com>
2021-05-17 17:37:18 +0800

    init

---
+++ RabbitMQ.Receive/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            IConnectionFactory connFactory = new ConnectionFactory//创建连接工厂对象
+            {
+                HostName = "localhost",//IP地址
+                Port = 5672,//端口号
+                UserName = "guest",//用户账号
+                Password = "guest"//用户密码
+            };
+            using (IConnection conn = connFactory.CreateConnection())
+            {
+                using (IModel channel = conn.CreateModel())
+                {
+                    String queueName = String.Empty;
+                    if (args.Length > 0)
+                        queueName = args[0];
+                    else
+                        queueName = "queue1";
+                    //声明一个队列
+                    channel.QueueDeclare(
+                      queue: queueName,//消息队列名称
+                      durable: false,//是否缓存
+                      exclusive: false,
+                      autoDelete: false,
+                      arguments: null
+                       );
+                    //创建消费者对象
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        byte[] message = ea.Body.ToArray();//接收到的消息
+                        Console.WriteLine("接收到信息为:" + Encoding.UTF8.GetString(message));
+                    };
+                    //消费者开启监听
+                    channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
