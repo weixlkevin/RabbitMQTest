9f8c1de358da08f0eac13735e39f27a525381b0d
weixl <weixingliang@gci-china.com>
2021-05-19 17:04:03 +0800

    提交

---
+++ RabbitMQ.Direct.Receive/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Direct.Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int random = new Random().Next(1, 1000);
+            Console.WriteLine($"Start.QueueName:{random}");
+            IConnectionFactory connFactory = new ConnectionFactory//创建连接工厂对象
+            {
+                HostName = "localhost",//IP地址
+                Port = 5672,//端口号
+                UserName = "guest",//用户账号
+                Password = "guest"//用户密码
+            };
+
+            using (IConnection conn = connFactory.CreateConnection())
+            {
+                using (IModel channel = conn.CreateModel())
+                {
+                    String exchangeName = String.Empty;
+                    if (args.Length > 0)
+                        exchangeName = args[0];
+                    else
+                        exchangeName = "exdirect";
+                    //channel.ExchangeDeclare(exchangeName, "fanout");
+
+                    string queueName = exchangeName + "_" + random;
+                    channel.QueueDeclare(queueName, true, false, false, null);
+
+                    channel.QueueBind(queueName, exchangeName, "GPS");
+                    channel.BasicQos(0, 1, false);
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+
+                        byte[] message = ea.Body.ToArray();//接收到的消息
+                        Console.WriteLine("接收到信息为:" + Encoding.UTF8.GetString(message));
+                        //返回消息确认
+                        channel.BasicAck(ea.DeliveryTag, true);
+                    };
+
+                    channel.BasicConsume(queueName, false, consumer);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQ.Direct.Send/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Direct.Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            IConnectionFactory connectionFactory = new ConnectionFactory() { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+            using (IConnection conn = connectionFactory.CreateConnection())
+            {
+                using (IModel channel = conn.CreateModel())
+                {
+                    string exchangeName = string.Empty;
+                    if (args.Length > 0)
+                    {
+                        exchangeName = args[0];
+
+                    }
+                    else
+                    {
+                        exchangeName = "exdirect";
+                    }
+                    channel.ExchangeDeclare(exchange: exchangeName, type: "direct", durable: true, autoDelete: false);
+                    while (true)
+                    {
+                        Console.WriteLine("消息内容:");
+                        string message = Console.ReadLine();
+                        byte[] body = Encoding.UTF8.GetBytes(message);
+                        channel.BasicPublish(exchange: exchangeName, routingKey: "GPS", basicProperties: null, body: body);
+                        Console.WriteLine($"成功发送消息:{message}");
+                    }
+                }
+            }
+        }
+    }
+}
