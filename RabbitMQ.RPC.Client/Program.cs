4f3485fd427d370683596917a4233713c78b74f2
weixl <weixingliang@gci-china.com>
2021-05-19 17:42:42 +0800

    新增rpc模式

---
+++ RabbitMQ.RPC.Client/Program.cs
@@ -0,0 +1,62 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.RPC.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client Start....");
+            IConnectionFactory connFactory = new ConnectionFactory//创建连接工厂对象
+            {
+                HostName = "localhost",//IP地址
+                Port = 5672,//端口号
+                UserName = "guest",//用户账号
+                Password = "guest"//用户密码
+            };
+
+            using (IConnection conn = connFactory.CreateConnection())
+            {
+                using (IModel channel = conn.CreateModel())
+                {
+                    //申明唯一guid用来标识此次发送的远程调用请求
+                    var correlationId = Guid.NewGuid().ToString();
+                    //申明需要监听的回调队列
+                    var replyQueue = channel.QueueDeclare().QueueName;
+                    var properties = channel.CreateBasicProperties();
+                    properties.ReplyTo = replyQueue;//指定回调队列
+                    properties.CorrelationId = correlationId;//指定消息唯一标识
+                    //string number = args.Length > 0 ? args[0] : "30";
+                    //var body = Encoding.UTF8.GetBytes(number);
+                    ////发布消息
+                    //channel.BasicPublish(exchange: "", routingKey: "rpc_queue", basicProperties: properties, body: body);
+                    //Console.WriteLine($"[*] Request fib({number})");
+                    // //创建消费者用于处理消息回调（远程调用返回结果）
+                    var callbackConsumer = new EventingBasicConsumer(channel);
+                    channel.BasicConsume(queue: replyQueue, autoAck: true, consumer: callbackConsumer);
+                    callbackConsumer.Received += (model, ea) =>
+                    {
+                        //仅当消息回调的ID与发送的ID一致时，说明远程调用结果正确返回。
+                        if (ea.BasicProperties.CorrelationId == correlationId)
+                        {
+                            var responseMsg = $"Get Response: {Encoding.UTF8.GetString(ea.Body.ToArray())}";
+                            Console.WriteLine($"[x]: {responseMsg}");
+                        }
+                    };
+                    while (true)
+                    {
+                        Console.WriteLine("消息内容:");
+                        string message = Console.ReadLine();
+                        byte[] body = Encoding.UTF8.GetBytes(message);
+                        channel.BasicPublish(exchange: "", routingKey: "rpc_queue", basicProperties: properties, body: body);
+                        Console.WriteLine($"[*] Request fib({message})");
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQ.RPC.Service/Program.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.RPC.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Serveice Start....");
+            IConnectionFactory connFactory = new ConnectionFactory//创建连接工厂对象
+            {
+                HostName = "localhost",//IP地址
+                Port = 5672,//端口号
+                UserName = "guest",//用户账号
+                Password = "guest"//用户密码
+            };
+
+            using (IConnection conn = connFactory.CreateConnection())
+            {
+                using (IModel channel = conn.CreateModel())
+                {
+                    //申明队列接收远程调用请求
+                    channel.QueueDeclare(queue: "rpc_queue", durable: false,
+                        exclusive: false, autoDelete: false, arguments: null);
+                    var consumer = new EventingBasicConsumer(channel);
+                    Console.WriteLine("[*] Waiting for message.");
+                    //请求处理逻辑
+                    consumer.Received += (model, ea) =>
+                    {
+                        var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                        int n = int.Parse(message);
+                        Console.WriteLine($"Receive request of Fib({n})");
+                        bool result = (n==1);
+                        //从请求的参数中获取请求的唯一标识，在消息回传时同样绑定
+                        var properties = ea.BasicProperties;
+                        var replyProerties = channel.CreateBasicProperties();
+                        replyProerties.CorrelationId = properties.CorrelationId;
+                        //将远程调用结果发送到客户端监听的队列上
+                        channel.BasicPublish(exchange: "", routingKey: properties.ReplyTo,
+                            basicProperties: replyProerties, body: Encoding.UTF8.GetBytes(result.ToString()));
+                        //手动发回消息确认
+                        channel.BasicAck(ea.DeliveryTag, false);
+                        Console.WriteLine($"Return result: {result}");
+                    };
+                    channel.BasicConsume(queue: "rpc_queue", autoAck: false, consumer: consumer);
+                    Console.ReadKey();
+                }
+            }
+            //申明唯一guid用来标识此次发送的远程调用请求
+         
+        }
+    }
+}
